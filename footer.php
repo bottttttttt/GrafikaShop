2c452916156145fbf37b59d317c986a9970d9617
Bottaeva Diana <dibottt42@gmail.com>
2023-12-22 20:32:56 +0300

    загрузка проекта

---
+++ footer.php
@@ -0,0 +1,60 @@
+</main>
+<!-- !start #main-site -->   
+  <!-- start #footer -->
+  <footer id="footer" class="text-white py-5 color-second-bg">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-12">
+          <h4 class="font-rubik font-size-20"><img src="../assets/logo2.svg"></h4>
+            <p class="font-size-14 font-rale text-white-50">Йоу. Это сайт с комиксами.</p>
+          </div>
+          <div class="col-lg-4 col-12">
+            <h4 class="font-rubik font-size-20">НОВОСТНАЯ РАССЫЛКА</h4>
+            <form class="form-row">
+              <div class="col">
+                <input type="text" class="form-control" placeholder="Email *">
+              </div>
+              <div class="col">
+                <button type="submit" class="btn btn-primary mb-2">Подписаться</button>
+              </div>
+            </form>
+          </div>
+          <div class="col-lg-2 col-12">
+            <h4 class="font-rubik font-size-20">ПОКУПАТЕЛЯМ</h4>
+            <div class="d-flex flex-column flex-wrap">
+              <a href="#" class="font-rale font-size-14 text-white-50 pb-1">О нас</a>
+              <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Доставка</a>
+              <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Контакты</a>
+              <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Возврат товара</a>
+            </div>
+          </div>
+          <div class="col-lg-2 col-12">
+            <h4 class="font-rubik font-size-20">Аккаунт</h4>
+            <div class="d-flex flex-column flex-wrap">
+              <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Мой Аккаунт</a>
+              <a href="#" class="font-rale font-size-14 text-white-50 pb-1">История заказов</a>
+              <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Список желаемого</a>
+              <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Рассылка</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- !start #footer -->
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+<!-- Owl Carousel Js file -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha256-pTxD+DSzIwmwhOqTFN+DB+nHjO4iAsbgfyFq5K5bcE0=" crossorigin="anonymous"></script>
+
+<!--  isotope plugin cdn  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js" integrity="sha256-CBrpuqrMhXwcLLUd5tvQ4euBHCdh7wGlDfNz8vbu/iI=" crossorigin="anonymous"></script>
+
+<!-- Custom Javascript -->
+<script src="index.js"></script>
+</body>
+</html>
